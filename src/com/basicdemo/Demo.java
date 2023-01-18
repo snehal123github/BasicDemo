554e3fd0f80bbac9c6e06e77583a7cf06f205be3
snehal <gulvesnehal123@gmail.com>
2023-01-18 20:07:56 +0530

    Basic demo class added

---
+++ src/com/basicdemo/Demo.java
@@ -0,0 +1,17 @@
+package com.basicdemo;
+
+import java.util.ArrayList;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		ArrayList list=new ArrayList();
+		list.add(11);
+		list.add(22);
+		list.add(33);
+		list.add(44);
+		System.out.println(list);
+
+	}
+
+}
