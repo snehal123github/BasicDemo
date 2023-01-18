5269a0e01522da1539915e1cf92f77080b3a0300
snehal <gulvesnehal123@gmail.com>
2023-01-18 22:21:42 +0530

    new class hashsetdemo added

---
+++ src/com/basicdemo/HashSetDemo.java
@@ -0,0 +1,18 @@
+package com.basicdemo;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetDemo {
+
+	public static void main(String[] args) {
+		Set set=new HashSet();
+		set.add(20);
+		set.add(30);
+		set.add(40);
+		System.out.println(set);
+		
+
+	}
+
+}
